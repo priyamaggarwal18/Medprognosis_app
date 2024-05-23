0569e7777d7e4db02aa328e4719359f41499d680
Priyam Aggarwal <122437027+priyamaggarwal18@users.noreply.github.com>
2024-05-24 01:09:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from flask import Flask, render_template, url_for ,request
-# from flask_login import UserMixin
-from flask_sqlalchemy import SQLAlchemy
-import random
 
 from static.breast_cancer import X_test, Y_test
 app = Flask(__name__)
